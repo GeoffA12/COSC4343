3ffd0bccaa1ee358ab1b419654bad4ff6406c7dd
GeoffA12 <Geoffarroyo@gmail.com>
2020-11-09 04:17:59 -0600

    Testing return.

--- login.php
+++ login.php
@@ -31,7 +31,7 @@
             // }
             $userArray = $result->fetch_assoc();
             if (!$userArray) {
-                echo "<h2>Invalid login</h2>";
+                return "Invalid login";
             }
             else {
                 echo $userArray["username"];
